078c98d8df17e4265ef48b1086aa1531ff0e8bb3
Imanina Majeed <imanina@recovision.net>
2023-04-05 11:38:36 +0800

    Add logo

--- frontend/src/layout/Logo.js
+++ frontend/src/layout/Logo.js
@@ -1,8 +1,8 @@
 import React from "react";
 import logo from "./recocloud.png";
 
-function Logo() {
-  return <img src={logo} alt="Logo" width="300" />;
+function Logo({ width, height }) {
+  return <img src={logo} alt="Logo" style={{ width, height }} />;
 }
 
 export default Logo;
