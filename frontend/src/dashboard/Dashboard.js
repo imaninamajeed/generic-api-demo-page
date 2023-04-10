5f6956a45a518fd8e65e0d6017e650c7b309c8fe
Imanina Majeed <imanina@recovision.net>
2023-04-10 14:56:18 +0800

    Update layout and sizing

--- frontend/src/dashboard/Dashboard.js
+++ frontend/src/dashboard/Dashboard.js
@@ -13,12 +13,12 @@ import "./Dashboard.css";
 const styles = {
   flex: { display: "flex" },
   flexColumn: { display: "flex", flexDirection: "column" },
-  leftCol: { flex: 2, marginRight: "0.5em" },
+  leftCol: { flex: 1, marginRight: "0.5em" },
   rightCol: { flex: 1, marginLeft: "0.5em" },
-  singleCol: { marginTop: "em", marginBottom: "1em" },
+  singleCol: { marginTop: "1em", marginBottom: "1em" },
 };
 
-const Spacer = () => <span style={{ width: "1em" }} />;
+// const Spacer = () => <span style={{ width: "1em" }} />;
 const VerticalSpacer = () => <span style={{ height: "1em" }} />;
 
 export const MyDashboard = () => {
