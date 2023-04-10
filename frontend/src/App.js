a71ddcaab6ead2b255a15f0d340218852cd486e6
Imanina Majeed <imanina@recovision.net>
2023-04-10 14:53:08 +0800

    Restructure export function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,26 +15,26 @@ import "@fontsource/poppins"; // Defaults to weight 400.
 const ffmpegIP = "172.17.11.7";
 console.log("ffmpegIP:", ffmpegIP, "connected");
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Admin
-            title="Traffic Offence"
-            dashboard={() => <MyDashboard ffmpegIP={ffmpegIP} />}
-            loginPage={MyLogin}
-            layout={MyLayout}
-            disableTelemetry
-          >
-            <Resource name="model_list" options={{ label: "Models" }} />
-          </Admin>
-        }
-      />
-      <Route path="/login" element={<MyLogin />} />
-    </Routes>
-  </BrowserRouter>
-);
-
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Admin
+              title="Traffic Offence"
+              dashboard={() => <MyDashboard ffmpegIP={ffmpegIP} />}
+              loginPage={MyLogin}
+              layout={MyLayout}
+              disableTelemetry
+            >
+              <Resource name="model_list" options={{ label: "Models" }} />
+            </Admin>
+          }
+        />
+        <Route path="/login" element={<MyLogin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
