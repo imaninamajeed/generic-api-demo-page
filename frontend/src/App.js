b89aca2182fe1c7a42f69f180602f55e34375553
Imanina Majeed <imanina@recovision.net>
2023-04-10 13:17:13 +0800

    Update imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,15 @@
-import { useState, useEffect } from "react";
-import { Admin, CustomRoutes, Resource } from "react-admin";
+// import libraries
+import { Admin, Resource } from "react-admin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import mqtt from "mqtt/dist/mqtt";
-import { MyLayout } from "./layout/Layout";
+
+// import components
 import { MyLogin } from "./layout/Login";
 import { MyDashboard } from "./dashboard/Dashboard";
 
-// import authProvider from './authProvider';
-import dataProvider from "./api/dataProvider";
-import { ModelList } from "./resources/ModelList";
-
+// import layout
+import { MyLayout } from "./layout/Layout";
 import "@fontsource/nunito"; // Defaults to weight 400.
 import "@fontsource/poppins"; // Defaults to weight 400.
-// import { lightTheme } from "./layout/themes";
 
 const ffmpegIP = "172.17.11.62";
 console.log("ffmpegIP:", ffmpegIP, "connected");
