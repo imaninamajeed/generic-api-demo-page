6ee34f2cf0b8ea8ffff9d46f215620a55883502d
Ho Zi Xuan <zixuanho@recogine.com>
2023-04-17 18:52:19 +0800

    feat: add auto deploy script

---
+++ frontend/.dev/auto-deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+echo "WARNING: FOR DEVELOPMENT PURPOSE ONLY! DO NOT USE OR COPY THIS SCRIPT TO PRODUCTION!"
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+## Stop all running docker containers
+echo "==============================================="
+echo "Stopping all running docker containers"
+echo "==============================================="
+"$SCRIPT_DIR"/stop-server.sh
+
+echo "==============================================="
+echo "Update to latest Git changes"
+echo "==============================================="
+git pull
+echo "Done git pulling"
+
+echo "==============================================="
+echo "Docker build the new images"
+echo "==============================================="
+"$SCRIPT_DIR"/build-docker.sh
+
+echo "==============================================="
+echo "Docker up all the containers"
+echo "==============================================="
+"$SCRIPT_DIR"/run-server.sh
+
+echo "[+] --- Finished deploying the dockers"
